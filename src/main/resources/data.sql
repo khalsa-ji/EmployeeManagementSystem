94ce3ee9370a6ec3adcc2c76f425bcc46225fa7f
RavikiranSingh <ravikiransingh@fico.com>
2019-10-30 10:25:02 +0530

    Restructured files

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,15 +8,3 @@ INSERT IGNORE INTO designation (designation, level_id) VALUES ('Developer', 4.0)
 INSERT IGNORE INTO designation (designation, level_id) VALUES ('DevOps', 4.0);
 INSERT IGNORE INTO designation (designation, level_id) VALUES ('QA', 4.0);
 INSERT IGNORE INTO designation (designation, level_id) VALUES ('Intern', 5.0);
-
---  Initialising employee table
--- SELECT * FROM employee;
--- INSERT IGNORE INTO employee (employee_name, job_title, manager_id, job_id) VALUES ('Black', 'Director', -1, 1);
--- INSERT IGNORE INTO employee (employee_name, job_title, manager_id, job_id) VALUES ('www', 'Manager', 1, 2);
--- INSERT IGNORE INTO employee (employee_name, job_title, manager_id, job_id) VALUES ('Black America', 'Intern', 1, 7);
--- INSERT IGNORE INTO employee (employee_name, job_title, manager_id, job_id) VALUES ('America', 'Intern', 2, 7);
--- INSERT IGNORE INTO employee (employee_name, job_title, manager_id, job_id) VALUES ('Black America1', 'Intern', 1, 7);
--- INSERT IGNORE INTO employee (employee_name, job_title, manager_id, job_id) VALUES ('Black America2', 'Intern', 1, 7);
--- INSERT IGNORE INTO employee (employee_name, job_title, manager_id, job_id) VALUES ('Black America3', 'Intern', 1, 7);
--- SELECT * FROM employee;
-
